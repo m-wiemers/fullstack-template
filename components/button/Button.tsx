b231210db5f10164ddbbcd3913d4a3205e73fe0a
m-wiemers <marcel@klebauter.de>
2021-03-11 12:02:58 +0100

    ➕ Add Storybook https://storybook.js.org/docs/react/get-started/install

---
+++ components/button/Button.tsx
@@ -0,0 +1,14 @@
+export type ButtonProps = {
+  primary: boolean;
+  label: string;
+};
+
+function Button({ primary, label, ...props }: ButtonProps) {
+  return (
+    <button className={`btn ${primary ? "primary" : ""}`} {...props}>
+      {label}
+    </button>
+  );
+}
+
+export default Button;
